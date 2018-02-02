81e14d93bbd3acf35e202e0513bad207b5f65c60
Jo-No <j.noble-smith@outlook.com>
2018-02-02 09:17:55 +0000

    all months added

--- app/src/main/java/com/example/jno14/moveggiesmoproblems/HomeFragment.kt
+++ app/src/main/java/com/example/jno14/moveggiesmoproblems/HomeFragment.kt
@@ -58,6 +58,15 @@ class HomeFragment : Fragment() {
             "01" -> "janurary"
             "02" -> "february"
             "03" -> "march"
+            "04" -> "april"
+            "05" -> "may"
+            "06" -> "june"
+            "07" -> "july"
+            "08" -> "august"
+            "09" -> "september"
+            "10" -> "october"
+            "11" -> "november"
+            "12" -> "december"
             else -> {
                 ""
             }
